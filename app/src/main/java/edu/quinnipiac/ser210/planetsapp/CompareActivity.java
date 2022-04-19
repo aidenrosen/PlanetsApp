e5ade1478c0844c466b563ce53ed543c2135f40c
jjnoga <jjnoga@quinnipiac.edu>
2022-04-19 16:56:20 -0400

    added API support to Compare Activity

--- app/src/main/java/edu/quinnipiac/ser210/planetsapp/CompareActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/planetsapp/CompareActivity.java
@@ -5,16 +5,16 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class DescriptionActivity extends AppCompatActivity {
+public class CompareActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_description);
+        setContentView(R.layout.activity_compare);
 
-        DescriptionFragment descFragment = new DescriptionFragment(this);
+        CompareFragment compFragment = new CompareFragment(this);
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_container, descFragment);
+        ft.replace(R.id.fragment_container, compFragment);
         ft.addToBackStack(null);
         ft.commit();
     }
